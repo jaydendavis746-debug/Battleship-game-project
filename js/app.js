12e64cea15f9d6a0afc5702ab7efa183feeeff14
Jayden Davis <jaydendavis746@gmail.com>
2025-12-17 20:32:22 +0000

    battleships game project

--- js/app.js
+++ js/app.js
@@ -44,22 +44,132 @@ for(let x = 0; x< width * width; x++ ){
     battlefield.append(field)
  }
  
- createBoard('pink', 'player 1');
- createBoard('red', 'player 2'); 
+ createBoard('pink', 'player-1');
+ createBoard('yellow', 'player-2'); 
 
- // createing ships
+ // creating ships
 
  class Ship{
     constructor(name , length){
         this.name= name
         this.length = length
     }
- }
+ };
  
- const destroyer = new Ship('destroyer', 2)
- const submarine = new Ship('submarine', 3)
- const cruiser = new Ship('cruiser',3)
- const battleships = new Ship('battleship',4)
- const carrier = new Ship('carrier', 5)
- const titanic = new Ship('titanic', 7)
+ const destroyer = new Ship('destroyer', 2);
+ const submarine = new Ship('submarine', 3);
+ const cruiser = new Ship('cruiser',3);
+ const battleships = new Ship('battleship',4);
+ const carrier = new Ship('carrier', 5);
+ const titanic = new Ship('titanic', 6);
  
+const ships =[destroyer,  cruiser, battleships, carrier, submarine, titanic];
+
+let notDropped;
+
+ 
+const addShipPiece = (user, ship, startId) =>{
+   
+    const allFieldBlocks = document.querySelectorAll(`#${user} div`)
+
+    let randomBoolean = Math.random() < 0.5;
+
+    let isHorizontal = user === 'player-1' ? angle === 0 : randomBoolean;
+
+    let randomStartIndex = Math.floor(Math.random() * width * width);
+
+    let startIndex = startId ? startId: randomStartIndex
+
+    let validStart = isHorizontal ? startIndex <= width * width - ship.length ? 
+    startIndex: width * width - ship.length:
+
+    //handle vertical
+
+ startIndex <= width * width - width*ship.length ? startIndex: 
+ startIndex - ship.length * width + width;
+
+   let shipBlocks= [];
+
+    for (let i = 0; i < ship.length; i++){
+        
+        if (isHorizontal){
+
+           shipBlocks.push(allFieldBlocks[Number(validStart) + i ]);
+
+        } else{
+
+        shipBlocks.push(allFieldBlocks[Number(validStart) + i * width])
+   }
+}
+ 
+let valid
+
+if(isHorizontal ){
+shipBlocks.every((_shipBlock, index)=>
+ valid = shipBlocks[0].id % width !== width -(shipBlocks.length - (index+1)))
+}
+else{
+shipBlocks.every((_shipBlock, index)=>
+valid = shipBlocks[0].id < 90 + (width * index + 1)
+    )
+}
+
+const notTaken = shipBlocks.every(shipBlock => !shipBlock.classList.contains('taken'))
+
+if ( valid && notTaken){
+  shipBlocks.forEach(shipBlock => {
+    
+    shipBlock.classList.add(ship.name);
+
+    shipBlock.classList.add('taken');
+})
+}
+else{
+    if( user === 'player-2') addShipPiece('player-2', ship)
+       else if(user === 'player-1') notDropped = true;
+}
+
+}
+
+
+ships.forEach(ship => addShipPiece('player-2', ship));
+
+// drag player ships
+
+let draggedFleet
+
+
+const dragStart = (e)=>{
+
+console.log('hello world')
+notDropped = false
+draggedFleet = e.target
+
+}
+
+const dragOver = (e)=>{
+e.preventDefault()
+
+}
+
+const dropFleet = (e)=>{
+
+    const startId = e.target.id
+    const ship = ships[draggedFleet.id]
+    addShipPiece('player-1', ship, startId)
+    if( !notDropped){
+        draggedFleet.remove()
+    }
+
+}
+
+const portFleets = Array.from(port.children)
+
+portFleets.forEach(portFleet => portFleet.addEventListener('dragstart', dragStart))
+
+const allplayer1Blocks = document.querySelectorAll('#player-1 div');
+
+allplayer1Blocks.forEach(player1Block =>{
+    player1Block.addEventListener('dragover', dragOver)
+     player1Block.addEventListener('drop', dropFleet)
+})
